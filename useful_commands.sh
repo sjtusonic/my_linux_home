9ec7d6137dbb7ba315f1aa48d30ce360c1700a5f
ubuntuServer64 <sonic@localhost.(none)>
2016-06-17 20:20:28 -0700

    add new file useful_commands.sh

---
+++ useful_commands.sh
@@ -0,0 +1,12 @@
+# useful commands to open necessary GUIs from terminal
+
+# change screen resolution:
+xrandr # print out 
+xrandr -s 1360x768 # change
+
+
+# window manager:
+gnome-wm
+
+# gnome full GUI version:
+gnome-shell
